c0ce5e9af89a1638e73d56a25ff66a3a8510c60c
solgg94 <solgalain@gmail.com>
2026-05-22 09:07:03 -0300

    endpoint reactivar paciente

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -12,11 +12,12 @@ export const turnosApi = {
 }
 
 export const pacientesApi = {
-  getAll:  ()          => axios.get(`${BASE_URL}/pacientes`),
-  getById: (id)        => axios.get(`${BASE_URL}/pacientes/${id}`),
-  create:  (data)      => axios.post(`${BASE_URL}/pacientes`, data),
-  update:  (id, data)  => axios.put(`${BASE_URL}/pacientes/${id}`, data),
-  delete:  (id)        => axios.delete(`${BASE_URL}/pacientes/${id}`)
+  getAll:      ()          => axios.get(`${BASE_URL}/pacientes`),
+  getById:     (id)        => axios.get(`${BASE_URL}/pacientes/${id}`),
+  create:      (data)      => axios.post(`${BASE_URL}/pacientes`, data),
+  update:      (id, data)  => axios.put(`${BASE_URL}/pacientes/${id}`, data),
+  delete:      (id)        => axios.delete(`${BASE_URL}/pacientes/${id}`),
+  reactivate:  (id)        => axios.put(`${BASE_URL}/pacientes/${id}/reactivate`)
 }
 
 export const medicosApi = {
